d9072dd460d7362e080c1edf3defc0ce7a703e87
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2020-02-11 11:52:27 -0700

    MLP quiz

---
+++ quiz/multilayer_perceptron_quiz_template.py
@@ -0,0 +1,26 @@
+import numpy as np
+import torch
+
+batch_size = 10_000
+input_size = 2
+hidden_size = 42
+output_size = 1
+lr = 0.01
+
+n_epochs = 10
+n_batches = 10
+
+
+def get_toy_data(batch_size):
+    LABELS = [0, 0, 1, 1]
+    CENTERS = [(-3, -3), (3, 3), (3, -3), (-3, 3)]
+    assert len(CENTERS) == len(LABELS), 'centers and labels have diff len'
+    x_data = []
+    y_targets = np.zeros(batch_size)
+    n_centers = len(CENTERS)
+    for batch_i in range(batch_size):
+        center_idx = np.random.randint(0, n_centers)
+        x_data.append(np.random.normal(loc=CENTERS[center_idx]))
+        y_targets[batch_i] = LABELS[center_idx]
+    return (torch.tensor(x_data, dtype=torch.float32),
+            torch.tensor(y_targets, dtype=torch.int64))
